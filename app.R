063e6e56da86e9f898e1def9a120ce0d46acb007
Hicham <h.m.safhi@gmail.com>
2020-05-24 21:42:21 +0000

    ++

--- app.R
+++ app.R
@@ -1,90 +1,106 @@
 
+#fighting-covid19-misinformation
+
 source('global.R')
 
 ui  = dashboardPage( skin = "green",
-  dashboardHeader(title = "Data Veracity"),
-  dashboardSidebar(
-    useShinyjs(),
-    sidebarMenu(id="mainmenu",
-                menuItem(text="Ask me", tabName = "ask_me"),
-                menuItem(text="About", tabName = "about")
-                )
-  ),
-  dashboardBody(
-    tabItems(
-      tabItem( tabName = 'ask_me',
-               box( status = "primary", width = 12
-                    , uiOutput("chathistory")
-                    , textInput("questioninput", label="", placeholder = "Type your question here, like: Is there a vaccin for  covid-19?")
-                    , actionButton("sendbutton", label = "Send")
-                    , tags$br(),tags$br(), tags$hr()
-                    , uiOutput("frequentlyasked_output")
-               )     
-      ),
-      tabItem( tabName = 'about',
-               box( status = "primary", width = 12
-                   , tags$h4("First version of a question/answering system (charbot), trained using raw text about covid-19 facts")
-                   , tags$h4("Current version uses data from Who website, FAQ section")
-               )
-      )
-      )
-    
-    
-  )
+                     dashboardHeader(title = "Data Veracity"),
+                     dashboardSidebar(
+                       useShinyjs(),
+                       sidebarMenu(id="mainmenu",
+                                   menuItem(text="Ask me", tabName = "ask_me"),
+                                   menuItem(text="About", tabName = "about")
+                       )
+                     ),
+                     dashboardBody(
+                       tabItems(
+                         tabItem( tabName = 'ask_me',
+                                   box( status = "primary", width = 12
+                                       , uiOutput("chathistory")
+                                       , textInput("questioninput", label="", placeholder = "Type your question here, like: Is there a vaccin for  covid-19?")
+                                       , htmlOutput("predicted_nextword")
+                                       , actionButton("sendbutton", label = "Send")
+                                       , tags$br(),tags$br(), tags$hr()
+                                       , uiOutput("frequentlyasked_output")
+                                  )
+                                  
+                                  
+                         ),
+                         tabItem( tabName = 'about',
+                                   box( id="Introduction",  status = "primary",  title = strong( 'Disclaimer' ), collapsible = T, width = 6, solidHeader = T,
+                                         h5("Myriad of data is generated every minute from everywhere, and in different formats offering the consumers a large availability of information . However, the reliability of this information differ highly across sources. Unreliable sources provide misleading and biased information, which might lead to dangerous consequences especially in the case of epidemics, like covid-19 in which falsehoods are a matter of life-and death. Users find it difficult to manually check all information, therefore, in this project we propose a question answering tool that can automatically answers the user using a pre-trained model based on reliable data sources. ")
+                                  )
+                                  
+                                  , box( id="disclaimer",  status = "primary",  title = strong( 'Disclaimer' ), collapsible = T, width = 6, solidHeader = T,
+                                         h5("We only provide guidance and information about the Covid Epidemic 19. We do not provide medical advice, in addition, the form is automatically trained from the data, and can sometimes provide different answers to that required.")
+                                  )
+                                  , box( status = "primary", width = 12
+                                       , tags$h4("First version of a question/answering system (charbot), trained using raw text about covid-19 facts")
+                                       , tags$h4("Current version uses data from Who website, FAQ section")
+                                  )
+                                  
+                         )
+                       )
+                       
+                       
+                     )
 )
-    
+
 
 
 server <- function(input, output, session) {
   
-    question = reactiveVal("")
-    
-    answer = reactive({
-      req(nchar(question()) != 0)
-      get_answer(question())
-    })
+  observeEvent(input$sendbutton, {
+    req(nchar(input$questioninput) != 0)
+    question = input$questioninput
+    answer = get_answer(question)
     
     output$chathistory = renderUI({
       HTML(paste0(
-        tags$b("You: "), tags$b(question()) , tags$br(),
-        tags$b("Answer: "), answer()
+        tags$b("You: "), tags$b(question) , tags$br(),
+        tags$b("Answer: "), answer
       )  )
     })
-    
-      
-    observeEvent(input$sendbutton, {
-      question(input$questioninput)
-      updateTextInput(session, inputId = "questioninput", value = "")
-    })
-    
-    ## frequently_questions is dynamically generated
-    output$frequentlyasked_output = renderUI(
-      {
-        questions_ui= tagList( tags$h4("Frequently asked questions:") , tags$br())
-        for(questionid in 1:nrow(frequently_questions)){
-          questions_ui = tagList(questions_ui
-                                 , actionButton(paste0("hint",frequently_questions[questionid, "questionid"])
-                                                , label = frequently_questions[questionid, "question"])
-                                 , tags$br(), tags$br()
-          )
-        }
-        
-        return(questions_ui)
-      }
-    )
-        
-        
-    observeEvent(input$hint1,{
-      question(frequently_questions[1, "question"])
-    })  
+    updateTextInput(session, inputId = "questioninput", value = "")
+  })
   
-    observeEvent(input$hint2,{
-      question(frequently_questions[2, "question"])
-    })  
+ 
+  output$predicted_nextword = renderText({
+    req(nchar(input$questioninput) != 0)
+    get_prediction(input$questioninput)$suggestion
+  })
+  
+  
+  ## frequently_questions is dynamically generated
+  output$frequentlyasked_output = renderUI(
+    {
+      questions_ui= tagList( tags$h4("Frequently asked questions:") , tags$br())
+      for(questionid in 1:nrow(frequently_questions)){
+        questions_ui = tagList(questions_ui
+                               , actionButton(paste0("hint",frequently_questions[questionid, "questionid"])
+                                              , label = frequently_questions[questionid, "question"])
+                               , tags$br(), tags$br()
+        )
+      }
+      
+      return(questions_ui)
+    }
+  )
+  
+  
+  observeEvent(input$hint1,{
+    question(frequently_questions[1, "question"])
+  })  
+  
+  observeEvent(input$hint2,{
+    question(frequently_questions[2, "question"])
+  })  
   
 }
-  
 
-shinyApp(ui, server)
 
-  
+myApp=shinyApp(ui, server)
+myApp
+#runApp(myApp,launch.browser=F, host="0.0.0.0", port=5081 )
+
+
--- global.R
+++ global.R
@@ -1,23 +1,115 @@
-
+.libPaths( c(.libPaths(), "/home/rstudio/R/x86_64-pc-linux-gnu-library/3.6") )
+  
 library(shiny)
 library(shinyjs)
 library(shinydashboard)
-library(ruimtehol)
+#library(ruimtehol)
+library(ngram)
+library(fastrtext)
+library(text2vec)
 
+#model = ruimtehol::starspace_load_model("model")
+if(!exists("model")){
+  model <- load_model("model")
+}
+kb = readRDS("data/facts_dataset.RDS")
+load('data/ngrams.RData')
+frequently_questions = readRDS("data/frequently_questions.RDS")
 
-model = ruimtehol::starspace_load_model("model")
-facts_dataset = readRDS("facts_dataset.RDS")
-    
-  
+# processig text: removing special characters and numbers, then steming: 
+process = function(strg){
+  strg = tolower(strg)
+  strg = gsub("[[:punct:]]", " ", strg)
+  strg = gsub("\\d"," ", strg)
+  strg  = trimws(strg)
+  strg = gsub("\\s+"," ",strg)
+  #strg = stemmer(strg)
+  return(strg)
+}
+
+sentence_similarity = function(sentence1, sentence2){
+  1 - crossprod(sentence1[1,],sentence2[1,])/sqrt(crossprod(sentence1[1,]) * crossprod(sentence2[1,]))
+}
+
 get_answer = function(question){
-  question = tolower(question)
-  question = gsub("[[:punct:]]", " ", question)
-  scores = predict(model, question ,basedoc = facts_dataset)
-  answer = scores[[1]]$prediction$label[1]
+  question = process(question)
+  query = get_sentence_representation(model,question)
+  dist_vect=c()
+  for(entry in 1:nrow(kb)){
+    dist_vect[entry] = sentence_similarity(query, kb[entry,"embd"])
+  }
+  print(min(dist_vect, na.rm = T))
+  if( min(dist_vect, na.rm = T) > 0.95){
+    answer = "I didn't understand the question, can you re-formulate it please?"
+  }else{
+    answer = kb[which.min(dist_vect) ,"V3" ]
+  }
   return(answer)
 }  
 
+stemmer <- function(x) {
+  tokens = word_tokenizer(x)
+  res= unlist(lapply(tokens, SnowballC::wordStem, language = "en"))
+  paste0(res,collapse = " ")
+}
 
-frequently_questions = data.frame(questionid=1:2, question=c(
-  "Is there a vaccine for covid19?", "How does COVID-19 spread ?"
-), stringsAsFactors = F)
+
+
+get_last_n_words <- function(input_string, n){
+  words <- unlist(strsplit(input_string, " "))
+  if(n > length(words)){return(input_string)}
+  nwords = paste(c(words[(length(words) - n + 1):(length(words))], ""), collapse = " ")
+  nwords = trimws(nwords)
+  nwords
+}
+
+get_punch <- function(ngramX, input_string){
+  punch_of_setup <- ngramX[ngramX$firstw == input_string, ]$nextw[1]
+  return(c(punch_of_setup))
+}
+
+get_prediction <- function(input_string) {
+  
+  res <- data.frame(current_string = process(input_string) , 
+                   suggestion = " ", 
+                   stringsAsFactors = FALSE)
+  
+  
+  if (res$current_string == ""){
+    return(res)
+  }
+  
+  # get ngram suggestions, try using 4-gram, then 3-gram, ..etc
+  res$current_string <- get_last_n_words(res$current_string, 4)
+  better_suggestion <- get_punch(ngram5, res$current_string)
+  if (!is.na(better_suggestion)){
+    res$suggestion <- better_suggestion
+    return(res)
+  }
+  
+  res$current_string <- get_last_n_words(res$current_string, 3)
+  better_suggestion <- get_punch(ngram4, res$current_string)
+  if (!is.na(better_suggestion)){
+    res$suggestion <- better_suggestion
+    return(res)
+  }
+  
+  res$current_string <- get_last_n_words(res$current_string, 2)
+  better_suggestion <- get_punch(ngram3, res$current_string)
+  if (!is.na(better_suggestion)){
+    res$suggestion <- better_suggestion
+    return(res)
+  }
+  
+  res$current_string <- get_last_n_words(res$current_string, 1)
+  better_suggestion <- get_punch(ngram2, res$current_string)
+  if (!is.na(better_suggestion)){
+    res$suggestion <- better_suggestion
+    return(res)
+  }
+  
+  return(res)
+  
+}
+
+
